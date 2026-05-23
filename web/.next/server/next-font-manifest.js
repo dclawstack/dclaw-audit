8a55b481234dfe6e2e0c83f000e2bb428a518fc5
chandraja12345 <shruthi675@gmail.com>
2026-05-23 11:21:32 +0530

    chore: remove .next and .vercel from repo, update web .gitignore
    - .next (Next.js build output) was accidentally committed
    - .vercel (Vercel CLI metadata) was accidentally committed
    - web/.gitignore now ignores both directories correctly

--- web/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/chandraja/AI_white_noise/dclaw/dclaw-audit/web/src/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
